f65ae736d4c7879a6a91f8475ecdd400f6693221
root <root@anil-pc.localdomain>
2025-11-10 08:10:22 +0530

    updated changes

--- boats.sh
+++ boats.sh
@@ -3,3 +3,6 @@
 docker compose down
 docker compose build --no-cache odoo-boats-app
 docker compose up --force-recreate --remove-orphans odoo-boats-app
+
+sleep 15
+sudo cp logs/* /mnt/c/Temp/odoo/logs
